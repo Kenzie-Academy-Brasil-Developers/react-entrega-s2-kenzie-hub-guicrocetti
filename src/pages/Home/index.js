787b98fdf3d81b969e84dd816585c264f4507533
guicrocetti <crocetti@outlook.com>
2022-06-16 20:01:07 -0300

    feat: modal add new

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,38 @@
 import { Redirect } from "react-router-dom";
 import Header from "../../components/Header";
 import { Container } from "./style";
+import {
+  Paper,
+  Box,
+  Stack,
+  Typography,
+  Grid,
+  IconButton,
+  styled,
+} from "@mui/material/";
+import RuleFolderIcon from "@mui/icons-material/RuleFolder";
+import AddBoxIcon from "@mui/icons-material/AddBox";
+import { useState } from "react";
+import ModalAdd from "../../components/Modal";
 
 function Home({ authenticated, setAuthenticate }) {
+  const [openModalAdd, setOpenModalAdd] = useState(false);
+
+  const handleOpen = () => {
+    setOpenModalAdd(true);
+    console.log(openModalAdd)
+  };
+
   if (!authenticated) {
     return <Redirect to={"/login"} />;
   }
+
+  const Item = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    color: theme.palette.text.secondary,
+  }));
+
   return (
     <>
       <Header logout={"logout"} setAuthenticate={setAuthenticate}></Header>
@@ -20,7 +47,56 @@ function Home({ authenticated, setAuthenticate }) {
             </p>
           </span>
         </div>
+        <Box className="Box" sx={{ width: "100%" }}>
+          <Paper elevation={0} className="Paper">
+            <Stack spacing={2}>
+              <Item className="Item">
+                <Grid
+                  justifyContent="space-between"
+                  alignItems="center"
+                  container
+                  padding="1rem"
+                >
+                  <h3>Tecnologias</h3>
+                  <IconButton
+                    aria-label="delete"
+                    size="small"
+                    onClick={() => handleOpen()}
+                  >
+                    <AddBoxIcon className="IconAdd" />
+                  </IconButton>
+                </Grid>
+              </Item>
+            </Stack>
+            <Stack spacing={2} className="Stack">
+              <Item className="Item">
+                <Grid justifyContent="flex-end" alignItems="center" container>
+                  <Grid item xs={9}>
+                    <h4>React JS</h4>
+                  </Grid>
+                  <Grid item xs={2}>
+                    <Typography>Filled</Typography>
+                  </Grid>
+                  <Grid item xs={1}>
+                    <IconButton
+                      aria-label="delete"
+                      size="small"
+                      onClick={() => {
+                        alert("clicado");
+                      }}
+                    >
+                      <RuleFolderIcon className="Icon" />
+                    </IconButton>
+                  </Grid>
+                </Grid>
+              </Item>
+              <Item className="Item">Item 2</Item>
+              <Item className="Item">Item 3</Item>
+            </Stack>
+          </Paper>
+        </Box>
       </Container>
+      <ModalAdd openModalAdd={openModalAdd} setOpenModalAdd={setOpenModalAdd} />
     </>
   );
 }
--- src/pages/Home/style.jsx
+++ src/pages/Home/style.jsx
@@ -6,9 +6,15 @@ export const Container = styled.div`
   align-items: center;
   justify-content: center;
   align-content: space-between;
-  gap: 2rem;
-  margin: 1rem auto;
-  height: 73.5%;
+  height: 100%;
+
+  h1,h2,h3,h4 {
+    color: white;
+  }
+
+  p {
+    color: var(--Gray-1);
+  }
 
   > div {
     flex-direction: row;
@@ -16,7 +22,75 @@ export const Container = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-evenly;
-    margin: 1rem auto;
-    height: 15.5%;
+    background-color: var(--Gray-4);
+    height: 118px;
+    border-top: 1px solid var(--Gray-2);
+    border-bottom: 1px solid var(--Gray-2);
   }
+
+  .Box {
+    background-color: var(--Gray-4);
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+    padding-top: 2rem;
+  }
+
+  .Paper {
+    background-color: var(--Gray-4);
+    width: 60%;
+  }
+
+  .Stack {
+    padding: 2rem;
+    background-color: var(--Gray-3);
+    width: 100%;
+  }
+
+  .Item {
+    background-color: var(--Gray-4);
+    padding: 1rem;
+    :hover {
+      background-color: var(--Gray-2);
+    }
+  }
+
+  .Icon, .IconAdd {
+    color: var(--Gray-1);
+    font-size: larger;
+    :hover {
+    cursor: pointer;
+    color: var(--Negative)
+  }
+  }
+
+  .IconAdd {
+    :hover {
+      color: var(--Success)
+    }
+  }
+
+  @media (max-width: 800px) {
+    
+    .Paper {
+    width: 80%;
+  }
+  .Icon, .IconAdd {
+      font-size: xx-large;
+    }
+
+  }
+
+  @media (max-width: 600px) {
+    .Paper {
+
+    width: 100%;
+  }
+  .Icon, .IconAdd {
+      font-size: xx-large;
+    }
+
+  }
+
 `;
